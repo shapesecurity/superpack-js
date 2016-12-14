fb6a37b1e5921bd06f96bade91bd063a13d01803
Michael Ficarra <github@michael.ficarra.me>
2016-12-14 13:03:00 -0800

    fixes #16: export encode/decode functions, not namespaces (#18)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 /* eslint-disable */
 
-var encode = require('./dist/cjs/encoder.js');
-var decode = require('./dist/cjs/decoder.js');
+var encode = require('./dist/cjs/encoder.js').default;
+var decode = require('./dist/cjs/decoder.js').default;
 
 module.exports = {
   encode: encode,
