78c0f54a1195ae089496b1753fc91eb5bc2b76c0
Tim Disney <tdisney@shapesecurity.com>
2016-12-15 17:19:24 -0800

    Clean up

--- test/browserstack.conf.js
+++ test/browserstack.conf.js
@@ -24,37 +24,37 @@ module.exports = function (config) {
       'bs_android_samsunggal44': {
         base: 'BrowserStack',
         os: 'Android',
-        os_version: 4.4,
+        'os_version': 4.4,
         browser: 'Android Browser',
         device: 'Samsung Galaxy S5'
       },
       'bs_iphone_91': {
         base: 'BrowserStack',
         os: 'iOS',
-        os_version: 9.3,
+        'os_version': 9.3,
         browser: 'iPhone',
         device: 'iPad Mini 4'
       },
       'bs_sierra_safari10': {
         base: 'BrowserStack',
         os: 'OS X',
-        os_version: 'Sierra',
+        'os_version': 'Sierra',
         browser: 'Safari',
-        browser_version: '10.0'
+        'browser_version': '10.0'
       },
       'bs_win10_ff50': {
         base: 'BrowserStack',
         os: 'Windows',
-        os_version: '10',
+        'os_version': '10',
         browser: 'Firefox',
-        browser_version: '50'
+        'browser_version': '50'
       },
       'bs_win10_chrome55': {
         base: 'BrowserStack',
         os: 'Windows',
-        os_version: '10',
+        'os_version': '10',
         browser: 'Chrome',
-        browser_version: '55'
+        'browser_version': '55'
       },
       'bs_win10_ie11': {
         base: 'BrowserStack',
