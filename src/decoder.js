6b7bb079a990d3b5a21262d7de7d1de0df1de118
Michael Ficarra <github@michael.ficarra.me>
2019-10-23 18:58:33 -0700

    simplify within implementation (#57)

--- src/decoder.js
+++ src/decoder.js
@@ -8,7 +8,7 @@ type Byte = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 
 type SuperPackedValue = Array<Byte>;
 
 function within(t, base, nBits) {
-  return t >= base && t < base + (1 << nBits);
+  return (t >>> nBits) == (base >>> nBits);
 }
 
 export default class Decoder extends Extendable {
